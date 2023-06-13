2f2d98da3a3429d98f1361d2a01958405b1a2915
Javier Oviedo <javieroviedo0512@gmail.com>
2023-06-13 16:04:32 -0300

    validacion

--- validaciones.js
+++ validaciones.js
@@ -1,50 +1,56 @@
-const email= document.getElementById("email");
-const password= document.getElementById("password");
-const nombre= document.getElementById("nombre");
-const apellido= document.getElementById("apellido");
-const nombreUsuario= document.getElementById("nombreUsuario");
-const ciudad= document.getElementById("ciudad");
-const algoSobreVos= document.getElementById("algoSobreVos");
-const form = document.getElementById("form");
-const parrafo= document.getElementById("warnings");
-
-form.addEventListener("submit", e=>{
-    e.preventDefault();
-    let warnings= "";
-    let regexEmail= /^\w([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/;
-    let entrar= false;
-    parrafo.innerHTML="";
-    if(!regexEmail.test(email.value)){
-        warnings += `El email no es valido <br>`
+document.addEventListener("DOMContentLoaded", function() {
+    const form = document.getElementById("form");
+    const parrafo = document.getElementById("warnings");
+  
+    form.addEventListener("submit", function(e) {
+      e.preventDefault();
+      let warnings = "";
+      let regexEmail = /^\w([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/;
+      let entrar = false;
+      parrafo.innerHTML = "";
+  
+      const email = document.getElementById("email");
+      const password = document.getElementById("password");
+      const nombre = document.getElementById("nombre");
+      const apellido = document.getElementById("apellido");
+      const nombreUsuario = document.getElementById("nombreUsuarioInput");
+      const ciudad = document.getElementById("ciudad");
+      const algoSobreVos = document.getElementById("algoSobreVos");
+  
+      if (!regexEmail.test(email.value)) {
+        warnings += `El email no es válido <br>`;
         entrar = true;
-    }
-    if(password.value.length <8) {
-        warnings += `La contraseña no es valida <br>`
+      }
+      if (password.value.length < 8) {
+        warnings += `La contraseña no es válida <br>`;
         entrar = true;
-    }
-    if(nombre.value.length < 3){
-        warnings += `El nombre no es valido <br>`
+      }
+      if (nombre.value.length < 3) {
+        warnings += `El nombre no es válido <br>`;
         entrar = true;
-    }
-    if(apellido.value.length < 3){
-        warnings += `El apellido no es valido <br>`
+      }
+      if (apellido.value.length < 3) {
+        warnings += `El apellido no es válido <br>`;
         entrar = true;
-    }
-    if(nombreUsuario.value.length < 3){
-        warnings += `El nombre de usuario no es valido <br>`
+      }
+      if (nombreUsuario.value.length < 3) {
+        warnings += `El nombre de usuario no es válido <br>`;
         entrar = true;
-    }
-    if(ciudad.value.length < 3){
-        warnings += `El nombre de la ciudad no es valido <br>`
+      }
+      if (ciudad.value.length < 3) {
+        warnings += `El nombre de la ciudad no es válido <br>`;
         entrar = true;
-    }
-    if(algoSobreVos.value.length < 20){
-        warnings += `Ingresa algo sobre vos <br>`
+      }
+      if (algoSobreVos.value.length < 20) {
+        warnings += `Ingresa algo sobre vos <br>`;
         entrar = true;
-    }
-    if(entrar){
-        parrafo.innerHTML=warnings;
-    }else{
-        parrafo.innerHTML="Enviado"
-    }
-})
+      }
+  
+      if (entrar) {
+        parrafo.innerHTML = warnings;
+      } else {
+        parrafo.innerHTML = "Enviado";
+      }
+    });
+  });
+  
